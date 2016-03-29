9c36316874ce90af86c5460266fb2351e6a8f6a0
OzzyThorne <xzzzf.dsx@gmail.com>
2016-03-29 23:05:02 +0800

    Added files via upload

---
+++ Prime.java
@@ -0,0 +1,33 @@
+import java.util.Scanner; 
+public class Prime
+{
+	public static void main(String[] args)
+	{
+		Scanner reader = new Scanner(System.in);
+		System.out.println("请输入需要判断的整数");
+		
+		int x = reader.nextInt(), i = 2 , m ;
+		boolean n = true;
+		
+		if (x < 2)
+		{
+			n = false;
+		}
+		else
+		{
+			for (;i < x ;i = i+1)
+			{
+				if(x % i == 0)
+				{
+					n = false;
+					break;
+				}
+			}
+		}
+		
+		if (n == true)
+			System.out.println("您输入的数为素数");
+		else
+			System.out.println("您输入的数不为素数");
+	}
+}
